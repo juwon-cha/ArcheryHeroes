e782f145331d5a7197496d10f3e8d10fdc38443f
Hira7388 <mine7388@gmail.com>
2025-07-29 20:45:54 +0900

    [Style] 룰 타일 제거

--- Assets/02.Scripts/Room/RoomData.cs
+++ Assets/02.Scripts/Room/RoomData.cs
@@ -3,23 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public enum TileType
-{
-    Empty,                  // 빈 공간
-    Floor,                  // 바닥
-    Wall,                   // 벽
-    Collision               // 나갈 수 없는 외곽선
-}
 public class RoomData : MonoBehaviour
 {
-    public int Width { get; private set; }
-    public int Height { get; private set; }
-    public TileType[,] TileGrid { get; private set; }
 
-    public RoomData(int width, int height)
-    {
-        this.Width = width;
-        this.Height = height;
-        this.TileGrid = new TileType[Width, Height]; // 설정한 크기로 방을 2차원 배열에 저장한다.
-    }    
 }
