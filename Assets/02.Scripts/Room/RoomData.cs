82fd8311b2db075c233989458065c5c040e474ca
Hira7388 <mine7388@gmail.com>
2025-07-29 20:14:34 +0900

    [Feat] 룰 타일 생성 중

--- Assets/02.Scripts/Room/RoomData.cs
+++ Assets/02.Scripts/Room/RoomData.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public enum TileType
+{
+    Empty,                  // 빈 공간
+    Floor,                  // 바닥
+    Wall,                   // 벽
+    Collision               // 나갈 수 없는 외곽선
+}
 public class RoomData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+    public TileType[,] TileGrid { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    public RoomData(int width, int height)
     {
-        
-    }
+        this.Width = width;
+        this.Height = height;
+        this.TileGrid = new TileType[Width, Height]; // 설정한 크기로 방을 2차원 배열에 저장한다.
+    }    
 }
