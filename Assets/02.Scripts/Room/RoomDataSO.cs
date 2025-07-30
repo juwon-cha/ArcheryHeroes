dfd444cb258a3dcaca78ad1c5f7f4ff1e42fbd5a
Hira7388 <mine7388@gmail.com>
2025-07-30 09:41:54 +0900

    [Feat] 기초 방 데이터 SO 파일
    추가로 Room 관련 스크립트 인코딩 utf-8로 변경

---
+++ Assets/02.Scripts/Room/RoomDataSO.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Data/Room")]
+public class RoomDataSO : ScriptableObject
+{
+    // 정적 데이터의 영역, 설계도 같은 개념
+    // 방 정보
+    public int width;
+    public int height;
+    public List<GameObject> obstaclePrefabs;
+    public int minObstacle;
+    public int maxObstacle;
+
+    // 몬스터 정보
+    public List<GameObject> enemyPrefabs; // 일단 임시 GameObject로 선언
+    public int count; // 몬스터 등장 마리수
+    public RoomType roomType;
+}
+
+public enum RoomType
+{
+    normal,
+    boss,
+    shop
+}
