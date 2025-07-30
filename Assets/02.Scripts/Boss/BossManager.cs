05f66fb81050f1dbf4306825b3461b6003eea99e
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-07-30 15:24:20 +0900

    중간저장

--- Assets/02.Scripts/Boss/BossManager.cs
+++ Assets/02.Scripts/Boss/BossManager.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossManager : MonoBehaviour
+public class BossManager : Singleton<BossManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
