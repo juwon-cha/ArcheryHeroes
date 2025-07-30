f6b051286dd666d7903cf29df1fbc1adb014bbcc
Hira7388 <mine7388@gmail.com>
2025-07-30 10:08:02 +0900

    [Style] 추가 스크립트 생성 시 utf-8 설정
    이제 스크립트를 추가하면 utf-8로 인코딩해서 한글 주석 안 깨지도록 합니다.

--- Assets/02.Scripts/Room/RoomDataSO.cs
+++ Assets/02.Scripts/Room/RoomDataSO.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class RoomDataSO : ScriptableObject
 {
     // 정적 데이터의 영역, 설계도 같은 개념
+    // !!중요!! 방 생성 방식 변경으로 안 쓸 것 같습니다.
+    // 추후에 확인하고 삭제하겠습니다.
     // 방 정보
     public int width;
     public int height;
