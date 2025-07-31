2d20d36996e010cb2f03384f43b9e17c78a22c69
Hira7388 <mine7388@gmail.com>
2025-07-31 15:09:26 +0900

    [Feat] 방 구조 변경 및 스크립트 변경

---
+++ Assets/02.Scripts/Room/DungeonManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonManager : Singleton<DungeonManager>
+{
+    [Header("방 프리팹 목록")]
+    [SerializeField] private List<GameObject> roomPrefabs;
+    [SerializeField] private GameObject bossRoomPrefab;
+
+    private GameObject currentRoomInstance; // 현재 방의 정보
+    private int currentStageIndex = 0; // 현재 스테이지 정보
+    private GameObject player; // 플레이어 정보
+    private Transform playerTransform; // 플레이어의 위치
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+        player = GameManager.Instance.Player;
+
+        if(player != null)
+        {
+            playerTransform = player.transform;
+        }
+
+        LoadNextRoom();
+    }
+
+    // 다음 방을 불러오는 메서드
+    public void LoadNextRoom()
+    {
+        currentStageIndex++;
+
+        // 방의 정보가 남아있다. 즉, 이전 방의 정보가 남아있다면
+        if(currentRoomInstance != null) 
+        {
+            // 이전 방을 삭제한다
+            Destroy(currentRoomInstance);
+        }
+
+        // Todo: 여기서 방 현재 스테이지에 따라 보스방 or 특별방 을 가져오는 조건 추가하면 된다.
+        // 방 프리팹중 한 개를 랜덤으로 가져온다.
+        GameObject roomToLoad = roomPrefabs[Random.Range(0, roomPrefabs.Count)];
+        // 가져온 방 프리팹을 생성하여 현재 방 정보에 넣어준다.
+        currentRoomInstance = Instantiate(roomToLoad, Vector3.zero, Quaternion.identity);
+
+        // 현재 방 정보에서 DungeonRoom 컴포넌트를 가져온다.
+        DungeonRoom newDungeonRoom = currentRoomInstance.GetComponent<DungeonRoom>();
+
+        if (newDungeonRoom != null && playerTransform != null)
+        {
+            // 플레이어의 포지션을 던전 방의 스폰 포인트 위치로 변경한다.
+            playerTransform.position = newDungeonRoom.GetPlayerSpawnPoint().position;
+        }
+    }
+}
