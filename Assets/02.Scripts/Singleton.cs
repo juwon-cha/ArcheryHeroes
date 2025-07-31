392297ce61de37c8be6c152fb35f644d7fcfa5af
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-07-31 16:06:22 +0900

    [Fix] 싱글톤 Awake에 virtual 추가

--- Assets/02.Scripts/Singleton.cs
+++ Assets/02.Scripts/Singleton.cs
@@ -23,7 +23,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 
     protected virtual void Initialize() { }
 
-    protected void Awake()
+    protected virtual void Awake()
     {
         if (instance != null && instance != this)
         {
