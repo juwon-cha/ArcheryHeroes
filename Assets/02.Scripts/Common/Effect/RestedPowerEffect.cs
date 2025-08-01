b84aa13df8a11b1308cb6c27f0366daee99b1715
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-08-01 20:32:42 +0900

    [Feat] 가만히 있으면 공격력, 공속증가 스킬 구현

--- Assets/02.Scripts/Common/Effect/RestedPowerEffect.cs
+++ Assets/02.Scripts/Common/Effect/RestedPowerEffect.cs
@@ -7,16 +7,23 @@ using UnityEngine;
 public class RestedPowerEffect : EffectSO
 {
     private PlayerController playerController;
+    private WeaponHandler weaponHandler;
 
     [SerializeField] private GameObject powerEffectPrefab;
     [SerializeField] private Color startColor;
     [SerializeField] private Color endColor;
 
     [SerializeField] private float maxRestedTime = 5f; // 최대 휴식 시간
-    [SerializeField] private float restedPowerMultiplier = 1.5f; // 1.5 = 150%
+    [Range(0f, 2f)]
+    [SerializeField] private float powerMultiplier = 0.5f; // 0.5 = 50% 데미지 증가
+    [Range(0f, .9f)]
+    [SerializeField] private float delayMultiplier = 0.5f; // 0.5 = 50% 공격 속도 감소
     private float currentRestedTime = 0f;
     private bool wasIdle = false; // 이전 프레임 상태 기억
     private ParticleSystem powerEffect;
+
+    private float originalPower;
+    private float originalDelay;
 
 
     public override void Initialize()
@@ -34,6 +41,10 @@ public class RestedPowerEffect : EffectSO
         {
             if (!wasIdle)
             {
+                weaponHandler = playerController.GetComponentInChildren<WeaponHandler>(true);
+
+                originalPower = weaponHandler.Power; // 원래 힘 저장
+                originalDelay = weaponHandler.Delay; // 원래 딜레이 저장
                 powerEffect = ObjectPoolingManager.Instance.Get(powerEffectPrefab, playerController.transform.position).GetComponent<ParticleSystem>();
                 var main = powerEffect.main;
                 main.loop = true; // 파티클 효과를 다시 시작
@@ -66,6 +77,12 @@ public class RestedPowerEffect : EffectSO
             var main = powerEffect.main;
             main.loop = false; // 파티클 효과 중지
         }
+        
+        if (weaponHandler != null)
+        {
+            weaponHandler.Power = originalPower; // 원래 힘으로 복원
+            weaponHandler.Delay = originalDelay; // 원래 딜레이로 복원
+        }
 
         currentRestedTime = 0f; // 휴식 시간 초기화
         wasIdle = false; // 상태 초기화
@@ -75,9 +92,11 @@ public class RestedPowerEffect : EffectSO
     void ApplyRestedPower()
     {
         float t = Mathf.Clamp(currentRestedTime / maxRestedTime, 0f, 1f);
-        float restedPower = t * restedPowerMultiplier; // 휴식 시간에 따른 힘 배수 적용
+        weaponHandler.Power = originalPower * (1f + t * powerMultiplier);
+        weaponHandler.Delay = originalDelay * (1f - t * delayMultiplier);
+
+        Debug.Log($"Rested Power: {weaponHandler.Power}, Delay: {weaponHandler.Delay}");
         SetEffectColor(t);
-        // playerController.ApplyRestedPower(restedPower);
     }
 
     void SetEffectColor(float t)
