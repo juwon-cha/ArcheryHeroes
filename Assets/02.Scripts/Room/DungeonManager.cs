0961a043d9a46902ae05ac101827b567647717e6
Hira7388 <mine7388@gmail.com>
2025-08-02 20:50:55 +0900

    [Fix] 보스방, 이벤트방 호출 변경

--- Assets/02.Scripts/Room/DungeonManager.cs
+++ Assets/02.Scripts/Room/DungeonManager.cs
@@ -7,6 +7,7 @@ public class DungeonManager : Singleton<DungeonManager>
     [Header("방 프리팹 목록")]
     [SerializeField] private List<GameObject> roomPrefabs;
     [SerializeField] private GameObject bossRoomPrefab;
+    [SerializeField] private GameObject eventRoomPrefab;
 
     private GameObject currentRoomInstance; // 현재 방의 정보
     private int currentStageIndex = 0; // 현재 스테이지 정보
@@ -41,7 +42,33 @@ public class DungeonManager : Singleton<DungeonManager>
 
         // Todo: 여기서 방 현재 스테이지에 따라 보스방 or 특별방 을 가져오는 조건 추가하면 된다.
         // 방 프리팹중 한 개를 랜덤으로 가져온다.
-        GameObject roomToLoad = roomPrefabs[Random.Range(0, roomPrefabs.Count)];
+        GameObject roomToLoad = null;
+
+        // 이번 스테이지가 10 단위 스테이지라면 (1순위)
+        if (currentStageIndex % 10 == 0)
+        {
+            if (bossRoomPrefab != null)
+            {
+                roomToLoad = bossRoomPrefab;
+            }
+        }
+
+        // 이번 스테이지가 5단위 스테이지라면 (2순위)
+        else if (currentStageIndex % 5 == 0)
+        {
+            if(eventRoomPrefab != null)
+            {
+                roomToLoad = eventRoomPrefab;
+            }
+        }
+
+        // 위 규칙에 해당하지 않는다면 일반 방을 호출한다.
+        if(roomToLoad == null)
+        {
+            roomToLoad = roomPrefabs[Random.Range(0, roomPrefabs.Count)];
+        }
+
+
         // 가져온 방 프리팹을 오브젝트 풀링으로 현재 방 정보에 넣어준다.
         currentRoomInstance = ObjectPoolingManager.Instance.Get(roomToLoad, Vector3.zero, Quaternion.identity);
 
