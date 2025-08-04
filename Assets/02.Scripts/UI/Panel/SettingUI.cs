de176138247ba505b7a5ae057b6a0d989e895048
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-08-04 18:02:33 +0900

    [Feat] 설정에 나가기 기능 추가 (튜토리얼, 플레이 씬에서만 활성화)

--- Assets/02.Scripts/UI/Panel/SettingUI.cs
+++ Assets/02.Scripts/UI/Panel/SettingUI.cs
@@ -1,15 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SettingUI : MonoBehaviour
 {
     private AudioSettingsPanel audioSettingsPanel;
+    [SerializeField] private GameObject exitButton;
 
     private void Awake()
     {
         audioSettingsPanel = GetComponentInChildren<AudioSettingsPanel>();
         audioSettingsPanel.Initialize();
+        exitButton.SetActive(false);
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode _)
+    {
+        if(scene.name == "TutorialScene" || scene.name == "PlayScene")
+        {
+            exitButton.SetActive(true);
+        }
+        else
+        {
+            exitButton.SetActive(false);
+        }
+    }
+
+
+    public void OnExit()
+    {
+        FadeManager.LoadScene("MainScene");
+        UIManager.Instance.HideUI(UIType.Setting);
     }
 
     public void OnResetSettings()
